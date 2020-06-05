60078438d16ce3927afcf4efaeb62bc394685155
Aslan <berezg86@gmail.com>
2020-06-05 11:36:47 +0300

    Шапка и баннер, выпадающее меню

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,3 +1,48 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+    var swiperBanner = new Swiper('.banner-slider', {
+        spaceBetween: 30,
+        effect: 'fade',
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            renderBullet: function(index, className) {
+                return '<span class="' + className + '">' + '0' + (index + 1) + '</span>';
+            },
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    var scrollTop = $(window).scrollTop();
+
+    if(scrollTop > 0){
+        $('.header').removeClass('header-opacity');
+    }
+
+    $(function() {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() >= 1) {
+                $('.header').removeClass('header-opacity');
+            } else {
+                $('.header').addClass('header-opacity');
+            }
+        });
+    });
+
+    $('.burger').click(function(){
+        $(this).toggleClass('is-active');
+        $('.dropdown-menu').toggleClass('is-active');
+    });
+
+    $('.close-menu').click(function(){
+        $('.burger').removeClass('is-active');
+        $('.dropdown-menu').removeClass('is-active');
+    });
+
+
+
 
 });
