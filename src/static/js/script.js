d20ea010fce8e50b3afbd4f8f5de4d8f9063916c
Aslan <berezg86@gmail.com>
2020-06-10 23:24:49 +0300

    Внутренняя страница

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,5 +1,87 @@
 $(document).ready(function() {
 
+    var mapEl = document.querySelector('#mapmain');
+
+    if (mapEl) {
+
+        ymaps.ready(init);
+
+        function init() {
+            var center = [55.59113656911934, 37.88662649999996];
+            var myMap = new ymaps.Map('mapmain', {
+                center: center,
+                controls: [],
+                zoom: 16
+            }, {
+                searchControlProvider: 'yandex#search'
+            });
+
+            myMap.behaviors.disable('scrollZoom');
+
+            var myPlacemark = new ymaps.Placemark(center, {
+                // Свойства.
+                // Содержимое иконки, балуна и хинта.
+                balloonContent: 'MOSCOW / SW1A 2AA, 10 Downing Street 5/2 9:00 - 18:00',
+                hintContent: 'MOSCOW / SW1A 2AA, 10 Downing Street 5/2 9:00 - 18:00'
+            }, {
+                // Опции.
+                iconLayout: 'default#image',
+                iconImageHref: 'img/baloon.png',
+                iconImageSize: [59, 81]
+                // preset: 'twirl#violetIcon'
+            });
+
+            myMap.geoObjects.add(myPlacemark);
+        }
+    }
+
+
+    $('.tab-links_desc a').click(function(e) {
+        e.preventDefault();
+        $('.tab-links_desc a').removeClass('is-active');
+        $(this).addClass('is-active');
+        var tab = $(this).attr('href');
+        $('.tab-block').not(tab).css({
+            'display': 'none'
+        });
+        $(tab).fadeIn(400);
+    });
+    $('.tab-links_desc a:first').click();
+
+    $('.service-tabs a').click(function(e) {
+        e.preventDefault();
+        $('.service-tabs a').removeClass('is-active');
+        $(this).addClass('is-active');
+        var tab = $(this).attr('href');
+        $('.service-info__tab').not(tab).css({
+            'display': 'none'
+        });
+        $(tab).fadeIn(400);
+    });
+    $('.service-tabs a:first').click();
+
+    $('.tab-links_mob').on('click', 'a', function(e) {
+        e.preventDefault();
+        $('.tab-links_mob a').removeClass('is-active');
+        $(this).addClass('is-active');
+        var tab = $(this).attr('href');
+        $('.tab-block').not(tab).css({
+            'display': 'none'
+        });
+        $(tab).fadeIn(400);
+    });
+
+    $('.service-tabs-mob').on('click', 'a', function(e) {
+        e.preventDefault();
+        $('.service-tabs-mob a').removeClass('is-active');
+        $(this).addClass('is-active');
+        var tab = $(this).attr('href');
+        $('.service-info__tab').not(tab).css({
+            'display': 'none'
+        });
+        $(tab).fadeIn(400);
+    });
+
     var galleryAbout = new Swiper('.about-slider', {
         spaceBetween: 10,
         observer: true,
@@ -47,8 +129,6 @@ $(document).ready(function() {
         }
     });
 
-
-
     var swiper = new Swiper('.scroll-slider', {
         direction: 'vertical',
         slidesPerView: 'auto',
@@ -70,6 +150,23 @@ $(document).ready(function() {
             499: {
                 slidesPerView: 2,
                 spaceBetween: 10,
+            }
+        }
+    });
+
+    var swiper = new Swiper('.service-list-slider', {
+        slidesPerView: 5,
+        spaceBetween: 15,
+        centeredSlides: true,
+        loop: true,
+        breakpoints: {
+            991: {
+                slidesPerView: 4,
+                spaceBetween: 15,
+            },
+            767: {
+                slidesPerView: 3,
+                spaceBetween: 15,
             }
         }
     });
@@ -126,6 +223,7 @@ $(document).ready(function() {
     });
 
     $('.select-cat').selectric();
+    // $('.time-select').selectric();
 
     var swiperBanner = new Swiper('.banner-slider', {
         spaceBetween: 30,
@@ -151,18 +249,26 @@ $(document).ready(function() {
         },
     });
 
+    var swiperBanner = new Swiper('.service-slider-mob', {
+        spaceBetween: 15,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
     var scrollTop = $(window).scrollTop();
 
     if (scrollTop > 0) {
-        $('.header').removeClass('header-opacity');
+        $('.header-main').removeClass('header-opacity');
     }
 
     $(function() {
         $(window).scroll(function() {
             if ($(this).scrollTop() >= 1) {
-                $('.header').removeClass('header-opacity');
+                $('.header-main').removeClass('header-opacity');
             } else {
-                $('.header').addClass('header-opacity');
+                $('.header-main').addClass('header-opacity');
             }
         });
     });
